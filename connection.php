e969a26fc1c5ebc1a12e64c4c264b38d59a7d3b9
matisoto27 <matiasfranciscosoto@hotmail.com>
2025-02-21 22:04:02 -0300

    MiniCorreccion connection.php

--- connection.php
+++ connection.php
@@ -4,3 +4,4 @@ $user = "if0_38338310";
 $password = "oEktUTvhsX4Jy";
 $database = "if0_38338310_tp_entornos_graficos";
 $mysqli = new mysqli($host, $user, $password, $database);
+mysqli_set_charset($mysqli, 'utf8mb4');
